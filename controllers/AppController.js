31261e0e06ba067448684b866042664effce428c
Adam Taylor <tayloradam1999@gmail.com>
2022-02-04 15:15:03 -0600

    Add all edits for checker + add anon async funcs thanks to Jay

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,26 +2,28 @@
 import RedisClient from '../utils/redis';
 import DBClient from '../utils/db';
 
-
 class AppController {
-	// using RedisClient method, checks if redis/db servers are running
-	static getStatus(req, res) {
-		const status = {
-			redis: RedisClient.isAlive(),
-			db: DBClient.isAlive()
-		};
-		return res.status(200).send(status);
-	}
+  // using RedisClient method, checks if redis/db servers are running
+  static getStatus(req, res) {
+    const status = {
+      redis: RedisClient.isAlive(),
+      db: DBClient.isAlive(),
+    };
+    return res.status(200).send(status);
+  }
 
-	// returns number of users and files in DB
-	static getStats(req, res) {
-		const stats = {
-			users: DBClient.nbUsers(),
-			files: DBClient.nbFiles()
-		};
-		return res.status(200).send(stats);
-	}
+  // returns number of users and files in DB
+  static getStats(req, res) {
+    (async () => {
+      const nbUsers = await DBClient.nbUsers();
+      const nbFiles = await DBClient.nbFiles();
+      return res.send({
+        users: nbUsers,
+        files: nbFiles,
+      });
+    }
+    )();
+  }
 }
-
 
 module.exports = AppController;
--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,57 +1,59 @@
+import { v4 as uuidv4 } from 'uuid';
 import RedisClient from '../utils/redis';
 import DBClient from '../utils/db';
-import { v4 as uuidv4 } from 'uuid';
-
 
 const sha1 = require('sha1'); // cryptographic hash function
 
 class AuthController {
-    
   static async getConnect(request, response) {
-    const AuthHeader = request.header('Authorization').split(' ');
+    // use authorization header to get the token, remove 'basic' from it
+    const header = request.headers.authorization;
 
-      // <email>:<password> will be split
-    const [email, password] = Buffer.from(header[1], 'utf8').toString('base64').split(':');
+    const ourStr = header.slice(6);
 
-      // If not found, return an error Unauthorized with a status code 401
-    if (!email)
-        return response.status(401).send({ error: 'Unauthorized' });
+    const object = Buffer.from(ourStr, 'base64'); // store the encoded str in the buffer
 
-    if(!password)
-	return response.status(401).send({ error: 'Unauthorized' });
-      
+    const convertedStr = object.toString('utf-8'); // decoded string
+
+    const [email, password] = convertedStr.split(':'); // <email>:<password> will be split   \
+
+    // If not found, return an error Unauthorized with a status code 401
+    if (!email) return response.status(401).send({ error: 'Unauthorized' });
+
+    if (!password) return response.status(401).send({ error: 'Unauthorized' });
+
     const hashedPassword = sha1(password); // store the SHA1 of the password
 
     const user = await DBClient.users.findOne({ email, password: hashedPassword });
-  
-      // If no user has been found, return an error Unauthorized with a status code 401
+
+    // If no user has been found, return an error Unauthorized with a status code 401
     if (!user) return response.status(401).send({ error: 'Unauthorized' });
 
     const token = uuidv4(); // used to generate random string as token
 
-      // Create a key: auth_<token>
+    // Create a key: auth_<token>
     const theKey = `auth_${token}`;
 
-      // Use this key for storing in Redis the user ID for 24 hours
+    // Use this key for storing in Redis the user ID for 24 hours
     await RedisClient.set(theKey, user._id.toString(), 60 * 60 * 24);
 
-      // Return this token: { "token": "155342df-2399-41da-9e8c-458b6ac52a0c" } with a status code 200
-    const specialTok = { token };
-    return response.status(200).send(specialTok);
+    const specialToken = { token };
+
+    return response.status(200).send(specialToken);
   }
 
-    static async getDisconnect(request, response) {
+  static async getDisconnect(request, response) {
     const theTok = request.header('X-token').split(' ');
 
-    const theKey = `auth_${token}`;
+    const theKey = `auth_${theTok}`;
 
     const user = await RedisClient.get(theKey);
 
-      // If not found, return an error Unauthorized with a status code 401
+    // If not found, return an error Unauthorized with a status code 401
     if (!user) return response.status(401).send({ error: 'Unauthorized' });
 
-      // Otherwise, delete the token in Redis and return nothing with a status code 204
-    RedisClient.del(key);
+    // Otherwise, delete the token in Redis and return nothing with a status code 204
+    RedisClient.del(theKey);
     return response.status(204).send();
   }
 }
