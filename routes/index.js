3baf4f2ba9d566fea45ac7fbf5549933242969cb
Adam Taylor <tayloradam1999@gmail.com>
2022-02-04 16:32:38 -0600

    Fix eslint

--- routes/index.js
+++ routes/index.js
@@ -8,23 +8,22 @@ const AuthController = require('../controllers/AuthController');
 
 // router setup
 const router = (app) => {
-	const paths = express.Router();
-	app.use(express.json());
-	app.use('/', paths);
+  const paths = express.Router();
+  app.use(express.json());
+  app.use('/', paths);
 
-	// GET '/status'
-	paths.get('/status', AppController.getStatus);
-	// GET '/stats'
-	paths.get('/stats', AppController.getStats);
-	// POST '/users'
-	paths.post('/users', UsersController.postNew);
-	// GET '/connect'
-	paths.get('/connect', AuthController.getConnect);
-	// GET '/disconnect'
-	paths.get('/disconnect', AuthController.getDisconnect);
-	// GET '/users/me'
-	paths.get('/users/me', UsersController.getMe);
+  // GET '/status'
+  paths.get('/status', AppController.getStatus);
+  // GET '/stats'
+  paths.get('/stats', AppController.getStats);
+  // POST '/users'
+  paths.post('/users', UsersController.postNew);
+  // GET '/connect'
+  paths.get('/connect', AuthController.getConnect);
+  // GET '/disconnect'
+  paths.get('/disconnect', AuthController.getDisconnect);
+  // GET '/users/me'
+  paths.get('/users/me', UsersController.getMe);
 };
-
 
 module.exports = router;
